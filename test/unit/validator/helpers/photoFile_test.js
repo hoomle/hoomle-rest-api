ad4446c8940d300bc36bc460595115e5bf12a845
Stan Chollet <stanislas.chollet@gmail.com>
2015-03-25 10:06:48 +0100

    refactor(validators): rename helper "file" to "photoFile"

---
+++ test/unit/validator/helpers/photoFile_test.js
@@ -0,0 +1,227 @@
+'use strict';
+
+var photoFileHelpers    = require('../../../../src/validator/helpers/photoFile'),
+    expect              = require('chai').expect,
+    sinon               = require('sinon');
+
+describe('validator / helpers / photoFile', function() {
+    describe('validator / helpers / photoFile / format()', function() {
+        it('should valid the format', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.format(filepath, 'jpeg')
+                .then(function(resolved) {
+                    expect(filepath)
+                        .to.be.equals(resolved);
+
+                    done();
+                });
+        });
+
+        it('should not valid the format', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.format(filepath, 'png')
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('format.not_supported');
+
+                    done();
+                });
+        });
+
+        it('should not valid the format (file is not an image)', function(done) {
+            var filepath = __dirname + '/fixtures/not_an_image.jpg';
+
+            photoFileHelpers.format(filepath, 'png')
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('format.not_supported');
+
+                    done();
+                });
+        });
+    });
+
+    describe('validator / helpers / photoFile / maxWidth()', function() {
+        it('should valid the width', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.maxWidth(filepath, 700)
+                .then(function(resolved) {
+                    expect(filepath)
+                        .to.be.equals(resolved);
+
+                    done();
+                });
+        });
+
+        it('should not valid the width', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.maxWidth(filepath, 600)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('width.too_high');
+
+                    done();
+                });
+        });
+
+        it('should not valid the width (file is not an image)', function(done) {
+            var filepath = __dirname + '/fixtures/not_an_image.jpg';
+
+            photoFileHelpers.maxWidth(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('width.too_high');
+
+                    done();
+                });
+        });
+    });
+
+    describe('validator / helpers / photoFile / maxHeight()', function() {
+        it('should valid the height', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.maxHeight(filepath, 700)
+                .then(function(resolved) {
+                    expect(filepath)
+                        .to.be.equals(resolved);
+
+                    done();
+                });
+        });
+
+        it('should not valid the height', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.maxHeight(filepath, 600)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('height.too_high');
+
+                    done();
+                });
+        });
+
+        it('should not valid the height (file is not an image)', function(done) {
+            var filepath = __dirname + '/fixtures/not_an_image.jpg';
+
+            photoFileHelpers.maxHeight(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('height.too_high');
+
+                    done();
+                });
+        });
+    });
+
+    describe('validator / helpers / photoFile / minHeight()', function() {
+        it('should valid the height', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.minHeight(filepath, 600)
+                .then(function(resolved) {
+                    expect(filepath)
+                        .to.be.equals(resolved);
+
+                    done();
+                });
+        });
+
+        it('should not valid the height', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.minHeight(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('height.too_low');
+
+                    done();
+                });
+        });
+
+        it('should not valid the height (file is not an image)', function(done) {
+            var filepath = __dirname + '/fixtures/not_an_image.jpg';
+
+            photoFileHelpers.minHeight(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('height.too_low');
+
+                    done();
+                });
+        });
+    });
+
+    describe('validator / helpers / photoFile / minWidth()', function() {
+        it('should valid the width', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.minWidth(filepath, 600)
+                .then(function(resolved) {
+                    expect(filepath)
+                        .to.be.equals(resolved);
+
+                    done();
+                });
+        });
+
+        it('should not valid the width', function(done) {
+            var filepath = __dirname + '/fixtures/discover-682x626.jpg';
+
+            photoFileHelpers.minWidth(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('width.too_low');
+
+                    done();
+                });
+        });
+
+        it('should not valid the width (file is not an image)', function(done) {
+            var filepath = __dirname + '/fixtures/not_an_image.jpg';
+
+            photoFileHelpers.minWidth(filepath, 700)
+                .then(null, function(err) {
+                    expect(err.filepath)
+                        .to.be.equals(filepath);
+
+                    expect(err.code)
+                        .to.be.equals('width.too_low');
+
+                    done();
+                });
+        });
+    });
+});
